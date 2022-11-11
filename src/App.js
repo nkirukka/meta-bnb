84e22c392d4f9568e9e68f4aa7f44b42c9d87e24
Nkiruka Ebele <specialebele@gmail.com>
2022-11-11 16:54:08 +0100

    feat: render navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Home, PlacesToStay, Nfts, Community } from './pages';
+import Navbar from './components/Navbar';
+
+
 function App() {
   return (
-    <div className="text-white font-black w-[80%] m-auto bg-red-500">
-    Metabnb
-    <p>ello</p>
+    <div>
+<Navbar/>
+
+      <Routes>
+        <Route path='/' element={ <Home/>} />
+        <Route path='/places-to-stay' element={ <PlacesToStay/>} />
+        <Route path='/nfts' element={ <Nfts/>} />
+        <Route path='/community' element={ <Community/>} />
+   </Routes>
     </div>
   );
 }
