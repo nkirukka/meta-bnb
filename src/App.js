c6f9677e166c1dc82925a813da3fc5d2c3ea7e19
Nkiruka Ebele <specialebele@gmail.com>
2022-11-11 11:03:40 +0100

    chore: update netlify-cli

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 function App() {
   return (
-    <div className="text-white font-black ml-6 bg-red-500">
+    <div className="text-white font-black w-[80%] m-auto bg-red-500">
     Metabnb
     <p>ello</p>
     </div>
