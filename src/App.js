0df14fbe01be1a599b6cdf47973f4bc34950a13f
Nkiruka Ebele <specialebele@gmail.com>
2022-11-11 10:37:28 +0100

    feat: test that tailwind is working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
+    <div className="text-white font-black ml-6 bg-red-500">
     Metabnb
+    <p>ello</p>
     </div>
   );
 }
