a7a1587dbc44ae54c5ae8fad8dbdc670c67dc181
Nkiruka Ebele <specialebele@gmail.com>
2022-11-12 10:13:20 +0100

    style: add styles for medium screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import navLogo from '../assets/nav-logo.svg';
 import hamburger from '../assets/icon-hamburger.svg';
 import closeIcon from '../assets/icon-close.svg';
@@ -29,20 +29,29 @@ const Navbar = () => {
     }
     return (
         <>
-            <section className='w-[85%] mx-auto my-[2em] flex justify-between relative'>
-                <img src={navLogo}
-                    alt='logo'
-                    title='Meta Logo'
-                    className='w-[100px]'
-                />
-                <nav className={`Nav ${btnChange ? 'active' : null} absolute left-[110%] top-[-2em] text-white w-[80vw] h-[100vh] pt-[6em] px-[1em] text-xl `} >
-                    <ul className='flex flex-col gap-8'>
+            <section className='w-[90%] max-w-[75em] mx-auto my-[2em] flex justify-between relative'>
+                
+                {/* ### NAV LOGO */}
+                <Link
+                    to='/'>
+                    <img
+                        src={navLogo}
+                        alt='logo'
+                        title='Meta Logo'
+                        className='w-[100px] md:w-[150px]'
+                    />
+                </Link>
+
+                {/* ### NAV MENU */}
+                <nav className={`Nav ${btnChange ? 'active' : null} absolute bg-[#A02279] left-[110%] top-[-2em] text-white w-[80vw] h-[100vh] pt-[6em] px-[1em] text-xl 
+                md:relative md:left-0 md:h-auto md:w-auto md:top-0 md:pt-0 md:text-[#434343] md:bg-transparent`} >
+                    <ul className='flex flex-col gap-8 md:flex-row'>
                         {navItems.map((item) => {
                             return <li key={item.name}>
                                 <NavLink
-                                    className={({ isActive }) => (isActive ? 'Active-nav' : 'hover:border-b-4 border-b-blue-600 transition-all')}
+                                    className={({ isActive }) => (isActive ? 'Active-nav whitespace-nowrap' : 'hover:border-b-4 border-b-blue-600 transition-all whitespace-nowrap')}
                                     to={item.link}
-                                onClick={()=>{setBtnChange(false)}}>
+                                    onClick={() => { setBtnChange(false) }}>
                                     {item.name}
                                 </NavLink>
                             </li>
@@ -50,18 +59,26 @@ const Navbar = () => {
                     </ul>
                 </nav>
 
-                <button type='button'>Connect wallet </button>
+                {/* ### CONNECT WALLET */}
+                <button
+                    type='button'
+                    className='bg-[#A02279] px-4 py-2 text-white rounded hover:bg-[#b62989] transition-all whitespace-nowrap'
+                >
+                    Connect wallet
+                </button>
+                
+                {/* ### TOGGLE NAV MENU */}
                 <button
                     type='button'
                     aria-label='Open nav menu'
                     role='Open'
                     onClick={handleClick}
-                    className='relative'
+                    className='relative md:hidden'
                 >
                     <img
                         src={btnChange ? closeIcon : hamburger}
                         alt={hamburger}
-                    className='w-[20px] h-[20px]'/>
+                        className='w-[15px] h-[15px]' />
                 </button>
             </section>
         </>
