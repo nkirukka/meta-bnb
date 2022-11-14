37cee08ad0932e53976fd9da1e9912a21af70c76
Nkiruka Ebele <specialebele@gmail.com>
2022-11-14 14:00:55 +0100

    style: add more margin and increase height to 60.5vh

--- src/pages/Nfts.js
+++ src/pages/Nfts.js
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 const Nfts = () => {
   return (
-    <section className="h-[50.5vh] text-center pt-20">
+    <section className="h-[60.5vh] w-[80%] m-auto text-center pt-20">
       <div className='max-w-3xl m-auto'>
-        <h2 className='text-9xl text-[#A02279] font-black mb-9'>SORRY</h2>
+        <h2 className='text-8xl md:text-9xl text-[#A02279] font-black mb-9'>SORRY</h2>
         <p className='font-black text-xl mb-5'>PAGE UNDER CONSTRUCTION</p>
         <p className='mb-10'>Please check back later.</p>
         <Link to='/' className='bg-[#A02279] py-3 px-6 text-white hover:bg-[#b62989] transition-colors'>GO TO HOMEPAGE</Link>
--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 const NoMatch = () => {
   return (
-      <section className="h-[50.5vh] text-center pt-20 mb-9">
+      <section className="h-[60.5vh] w-[80%] m-auto text-center pt-20 mb-9">
           <div className='max-w-3xl m-auto'>
-              <h2 className='text-9xl text-[#A02279] font-black mb-9'>Oops!</h2>
+              <h2 className=' text-8xl md:text-9xl text-[#A02279] font-black mb-9'>Oops!</h2>
               <p className='font-black text-xl mb-5'>404 - PAGE NOT FOUND</p>
               <p className='mb-10'>The page you are looking for might been removed, had its name changed or is temporarily unavailable.</p>
               <Link to='/' className='bg-[#A02279] py-3 px-6 text-white hover:bg-[#b62989] transition-colors'>GO TO HOMEPAGE</Link>
