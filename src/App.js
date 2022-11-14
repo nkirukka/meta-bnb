8156a36fe08e6c7a7c1e6e619084ab4cedf81a9d
Nkiruka Ebele <specialebele@gmail.com>
2022-11-14 12:39:06 +0100

    feat: move footer to /App so other pages can have access

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Home, PlacesToStay, Nfts, Community } from './pages';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import NoMatch from './pages/NoMatch';
 
 
 function App() {
   return (
     <div>
-<Navbar/>
+      <Navbar />
 
       <Routes>
-        <Route path='/' element={ <Home/>} />
-        <Route path='/places-to-stay' element={ <PlacesToStay/>} />
-        <Route path='/nfts' element={ <Nfts/>} />
-        <Route path='/community' element={ <Community/>} />
-        <Route path='*' element={ <NoMatch/>} />
-   </Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/places-to-stay' element={<PlacesToStay />} />
+        <Route path='/nfts' element={<Nfts />} />
+        <Route path='/community' element={<Community />} />
+        <Route path='*' element={<NoMatch />} />
+      </Routes>
+      
+      <Footer />
     </div>
   );
 }
