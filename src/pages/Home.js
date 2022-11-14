5da7e248e2201a401842022d9294dbec9d33971b
Nkiruka Ebele <specialebele@gmail.com>
2022-11-14 12:40:22 +0100

    refactor: move footer to App

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import Hero from "../components/Hero";
 import CardContainer from "../components/CardContainer";
 import Cta from "../components/Cta";
-import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -9,7 +8,6 @@ const Home = () => {
       <Hero />
       <CardContainer />
       <Cta />
-      <Footer />
     </>
   )
 }
