44d17a390a2ae037c26d3ff294202b836b751e93
Nkiruka Ebele <specialebele@gmail.com>
2022-11-14 08:51:56 +0100

    style: make font size larger on medium screens and align search input and buttonside by side

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,18 +9,21 @@ const Hero = () => {
         <section>
             <div className='flex flex-col-reverse w-[90%] mx-auto mt-24 '>
                 <div>
-                    <h1 className='text-center text-3xl my-8 text-[#434343]'>Rent a <span className='font-semibold text-[#A02279] '>Place </span>
+                    <h1 className='text-center text-3xl my-8 text-[#434343] md:text-5xl'>Rent a <span className='font-semibold text-[#A02279] '>Place </span>
                         away from <span className='font-semibold text-[#A02279] '>Home</span> in the <span className='font-semibold text-[#A02279] '>Metaverse</span></h1>
-                    <p className='text-center text-[#434343] mb-8'>
+                    <p className='text-center text-[#434343] mb-8 md:text-xl'>
                         We provide you access to luxury and affordable
                         houses in the metaverse, get a chance to turn
                         your imagination to reality at your comfort zone
                     </p>
 
                 {/* ### SEARCH */}
-                    <form>
-                        <input type='text' placeholder='Search for location' className='border-[1px] w-[100%] border-[#A3A3A3] py-2 pl-2 mb-2 rounded' />
-                        <button type="submit" className='w-[100%] bg-[#A02279] text-white rounded-md py-2 hover:bg-[#b62989] transition-colors '>Search</button>
+                    <form className='md:flex'>
+                        <input type='text'
+                            placeholder='Search for location'
+                            className='border-[1px] w-[100%] border-[#A3A3A3] py-2 pl-2 mb-2 rounded md:h-14 md:rounded-tr-none md:rounded-br-none md:w-[60%] md:border-r-0' />
+                        <button type="submit"
+                            className='w-[100%] bg-[#A02279] text-white rounded-md py-2 hover:bg-[#b62989] transition-colors md:h-14 md:rounded-tl-none md:rounded-bl-none md:w-[40%] '>Search</button>
                     </form>
                 </div>
 
