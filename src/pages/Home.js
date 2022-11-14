69556173db77e6494015355409b765c458b282e0
Nkiruka Ebele <specialebele@gmail.com>
2022-11-14 10:44:13 +0100

    feat: render footer section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
 import Hero from "../components/Hero";
 import CardContainer from "../components/CardContainer";
 import Cta from "../components/Cta";
-
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <CardContainer />
-      <Cta/>
+      <Cta />
+      <Footer />
     </>
   )
 }
