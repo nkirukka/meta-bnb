cb6a87ae05d009956b24610f345b90c9f484553a
Nkiruka Ebele <specialebele@gmail.com>
2022-11-14 09:11:34 +0100

    feat: render cta section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
 import Hero from "../components/Hero";
 import CardContainer from "../components/CardContainer";
-
+import Cta from "../components/Cta";
 
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <CardContainer/>
+      <CardContainer />
+      <Cta/>
     </>
   )
 }
