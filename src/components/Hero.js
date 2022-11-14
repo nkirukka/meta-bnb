f5977ad0938398cdfd86db6c208fba53b9b00146
Nkiruka Ebele <specialebele@gmail.com>
2022-11-14 10:16:39 +0100

    style: make hero reponsive on medium and up screens

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,11 +7,13 @@ import openSea from '../assets/open-sea.svg';
 const Hero = () => {
     return (
         <section>
-            <div className='flex flex-col-reverse w-[90%] mx-auto mt-24 '>
-                <div>
-                    <h1 className='text-center text-3xl my-8 text-[#434343] md:text-5xl'>Rent a <span className='font-semibold text-[#A02279] '>Place </span>
+            <div className='flex flex-col-reverse w-[90%] mx-auto mt-24 
+            md:flex-row md:justify-center md:items-center md:px-1 md:gap-6'>
+                <div className='md:w-[50%] xl:px-24 '>
+                    <h1 className='text-center text-3xl my-8 text-[#434343] 
+                    md:text-5xl md:text-left'>Rent a <span className='font-semibold text-[#A02279] '>Place </span>
                         away from <span className='font-semibold text-[#A02279] '>Home</span> in the <span className='font-semibold text-[#A02279] '>Metaverse</span></h1>
-                    <p className='text-center text-[#434343] mb-8 md:text-xl'>
+                    <p className='text-center md:text-left text-[#434343] mb-8 md:text-xl'>
                         We provide you access to luxury and affordable
                         houses in the metaverse, get a chance to turn
                         your imagination to reality at your comfort zone
@@ -27,10 +29,10 @@ const Hero = () => {
                     </form>
                 </div>
 
-                <div className='max-w-[400px] mx-auto'><img src={heroImg} alt='banner' title='Home preview' /></div>
+                <div className='max-w-[400px] mx-auto md:max-w-[50%]'><img src={heroImg} alt='banner' title='Home preview' /></div>
             </div>
 
-            <footer className='flex flex-col max-h-32 mt-10 Hero-footer p-3 bg-[#A02279]'>
+            <footer className='flex flex-col max-h-32 mt-10 Hero-footer p-3 bg-[#A02279] md:flex-row'>
                 <img src={mbToken} alt='MB Token' className='' />
                 <img src={metaMask} alt='Meta mask' className='max-w-16' />
                 <img src={openSea} alt='Open sea' className='max-w-16' />
