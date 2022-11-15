6f6ad18658e317a03d7aa581e12b12149786545e
Nkiruka Ebele <specialebele@gmail.com>
2022-11-15 21:05:05 +0100

    feat: accept lifted state to close modal

--- src/components/OverlayWallet.js
+++ src/components/OverlayWallet.js
@@ -5,13 +5,18 @@ import Wallet from './Wallet';
 import foxIcon from '../assets/fox.png';
 import walletIcon from '../assets/wallet-connect.png';
 
-const OverlayWallet = () => {
+const OverlayWallet = (props) => {
+    const handleClick = () =>{
+        props.open(false);
+    }
     return (
-        <section className='bg-[#0f00007a] absolute top-0 bottom-0 left-0 right-0 pt-40'>
+        <section
+            onClick={handleClick}
+            className='bg-[#0f00007a] absolute top-0 bottom-0 left-0 right-0 pt-40'>
             <div className='bg-white w-[80%] max-w-[25em] mx-auto rounded-2xl'>
                 <header className='flex w-[100%] justify-between py-4 px-6 border-b border-[#CFD8DC]'>
                     <h4 className='font-black'>Connect Wallet</h4>
-                    <span><FontAwesomeIcon className='text-[#575454]' icon={faClose} /></span>
+                    <span><FontAwesomeIcon onClick={handleClick} className='text-[#575454] cursor-pointer' icon={faClose} /></span>
                 </header>
 
                 <div className='px-6 py-4' >
