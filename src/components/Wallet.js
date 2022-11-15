fc18727f7d35137862e7fc53e821bcea07bba13a
Nkiruka Ebele <specialebele@gmail.com>
2022-11-15 16:55:47 +0100

    style: add styles for wallet

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -4,9 +4,9 @@ import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const Wallet = ({src, alt, name}) => {
   return (
-      <div>
-          <span> <img src={src} alt={alt} /><p>{name}</p> </span>
-          <FontAwesomeIcon icon={faChevronRight}/>
+      <div className=' flex w-[300px] justify-between items-center px-6 py-2 mb-2 border border-[#CFD8DC] rounded-lg'>
+          <span className='flex items-center'> <img src={src} alt={alt} className='w-7 mr-2' /><p className='font-[Sora] font-black text-sm'>{name}</p> </span>
+          <FontAwesomeIcon className='text-[#959DA6]' icon={faChevronRight}/>
     </div>
   )
 }
