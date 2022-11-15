65fa5ceceb8e818fb8f91f94a13e7274d7fb2002
Nkiruka Ebele <specialebele@gmail.com>
2022-11-15 17:21:32 +0100

    style: add max-width and change overlay background color

--- src/components/OverlayWallet.js
+++ src/components/OverlayWallet.js
@@ -7,8 +7,8 @@ import walletIcon from '../assets/wallet-connect.png';
 
 const OverlayWallet = () => {
     return (
-        <section className='bg-gray-200'>
-            <div className='bg-white w-[25em] mx-auto rounded-2xl'>
+        <section className='bg-[#9790903a] absolute top-0 bottom-0 left-0 right-0 pt-40'>
+            <div className='bg-white w-[80%] max-w-[25em] mx-auto rounded-2xl'>
                 <header className='flex w-[100%] justify-between py-4 px-6 border-b border-[#CFD8DC]'>
                     <h4 className='font-black'>Connect Wallet</h4>
                     <span><FontAwesomeIcon className='text-[#575454]' icon={faClose} /></span>
