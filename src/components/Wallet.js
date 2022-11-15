47d9c4ff44f6555daeaf172546e903b36bd9b57a
Nkiruka Ebele <specialebele@gmail.com>
2022-11-15 16:59:22 +0100

    style: reduce chevron size

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -6,7 +6,7 @@ const Wallet = ({src, alt, name}) => {
   return (
       <div className=' flex w-[300px] justify-between items-center px-6 py-2 mb-2 border border-[#CFD8DC] rounded-lg'>
           <span className='flex items-center'> <img src={src} alt={alt} className='w-7 mr-2' /><p className='font-[Sora] font-black text-sm'>{name}</p> </span>
-          <FontAwesomeIcon className='text-[#959DA6]' icon={faChevronRight}/>
+          <FontAwesomeIcon className='text-[#959DA6] text-sm' icon={faChevronRight}/>
     </div>
   )
 }
